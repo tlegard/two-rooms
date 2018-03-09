134afab9148e2dcc70172b8c7ac6cd443152d258
Tucker Legard <tlegard@vt.edu>
2018-03-09 13:52:14 -0500

    More heroku fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,6 @@
+import * as path from "path";
+import * as serveIndex from "serve-index";
+
 import { BeginnerGame } from "./game";
 import * as express from "express";
 import { createServer } from "http";
@@ -12,5 +15,9 @@ const gameServer = new Server({ server: httpServer });
 
 gameServer.register("beginner", BeginnerGame);
 gameServer.listen(port);
+
+console.log(path.join(__dirname, "../../client", "build"));
+app.use(express.static(path.join(__dirname, "../../client", "build")));
+app.use("/", serveIndex(path.join(__dirname, "../../client"), { icons: true }));
 
 console.log(`Listening on http://localhost:${port}`);
